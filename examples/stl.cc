931ba8268fab431612ab405c7bb2443636eecd11
eschkufz <eric.schkufza@gmail.com>
2011-12-11 02:32:01 -0800

    Fixed tabs.

--- examples/stl.cc
+++ examples/stl.cc
@@ -13,12 +13,12 @@ using namespace std;
 
 int main()
 {
-	list<int> l;
-	for ( unsigned int i = 0; i < 4096; ++i )
-		l.push_back(i);
+  list<int> l;
+  for ( unsigned int i = 0; i < 4096; ++i )
+    l.push_back(i);
 
-	for ( list<int>::iterator i = l.begin(), ie = l.end(); i != ie; ++i )
-		cout << *i << endl;
+  for ( list<int>::iterator i = l.begin(), ie = l.end(); i != ie; ++i )
+    cout << *i << endl;
 
   vector<int> xs;
   for ( unsigned int i = 0; i < 4096; ++i )
@@ -27,17 +27,17 @@ int main()
   for ( unsigned int i = 0; i < 4096; ++i )
     cout << xs[i] << endl;
 
-	string s;
-	map<int, string> m;
+  string s;
+  map<int, string> m;
 
-	for ( unsigned int i = 0; i < 1024; ++i )
-	{
-		s += "x";
-		m[i] = s;
-	}	
+  for ( unsigned int i = 0; i < 1024; ++i )
+  {
+    s += "x";
+    m[i] = s;
+  }  
 
-	for ( map<int, string>::iterator i = m.begin(), ie = m.end(); i != ie; ++i )
-		cout << (*i).first << " " << (*i).second << endl;
+  for ( map<int, string>::iterator i = m.begin(), ie = m.end(); i != ie; ++i )
+    cout << (*i).first << " " << (*i).second << endl;
 
   return 0;
 }
